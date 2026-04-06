fa8c0ae1c0080948de137a7722290e3f23cd7840
Shivanshu Prajapati <shivanshuprajapati212@gmail.com>
2026-04-06 20:51:09 +0530

    v0.2

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ import (
 	"golang.org/x/term"
 )
 
-var TEXT string = "Shivanshu"
+var TEXT string = "shivanshu"
 
 func main() {
 	width, height, err := term.GetSize(int(os.Stdout.Fd()))
@@ -19,19 +19,29 @@ func main() {
 		panic(err)
 	}
 
-	fig := figure.NewFigure(TEXT, "block", true)
+	fig := figure.NewFigure(TEXT, "", true)
 	lines := fig.Slicify()
 
 	longest := slices.MaxFunc(lines, func(a, b string) int {
 		return cmp.Compare(len(a), len(b))
 	})
 
+	fmt.Print("\033[?25l")
+
+	lllll := 0
 	for {
 		fmt.Print("\033[2J")
 
 		for i, v := range lines {
 			fmt.Printf("\033[%d;%dH", ((height/2)-len(lines)/2)+i, (width/2)-len(longest)/2)
-			fmt.Print(v)
+			for _, letter := range v {
+				fmt.Printf("\033[38;2;%d;%d;%dm%s\033[0m", lllll, lllll, lllll, string(letter))
+				if lllll >= 255 {
+					lllll = 0
+				} else {
+					lllll += 1
+				}
+			}
 		}
 
 		time.Sleep(time.Second / 24)
