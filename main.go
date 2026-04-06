6971ccafe59e5ade4fe2c535ba9c795028b9b6d7
Shivanshu Prajapati <shivanshuprajapati212@gmail.com>
2026-04-06 16:58:41 +0530

    add term width and height

--- main.go
+++ main.go
@@ -1,4 +1,18 @@
 package main
 
+import (
+	"os"
+
+	"golang.org/x/term"
+)
+
+var TEXT string = "Shivanshu"
+
 func main() {
+	width, height, err := term.GetSize(int(os.Stdout.Fd()))
+	if err != nil {
+		panic(err)
+	}
+
+	print(width, height)
 }
